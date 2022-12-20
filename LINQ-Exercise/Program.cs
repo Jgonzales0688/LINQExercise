ebc3db67ad6c82c76a123b3b4c60eb36746e23d7
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-20 11:07:44 -0600

    Exercise 2

---
+++ LINQ-Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Collections.Generic;
+
+List<string> gameName = new List<string>();
+
+gameName.Add("Spyro");
+gameName.Add("Halo");
+gameName.Add("Lost in Random");
+gameName.Add("Super Mario");
+gameName.Add("Fortnite");
+gameName.Add("Crash Bandicoot");
+
+gameName.OrderByDescending(x => x.Length).ToList().ForEach(x => Console.WriteLine(x)); 
+
